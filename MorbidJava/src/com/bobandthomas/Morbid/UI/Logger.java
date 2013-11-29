9d508c8382e7be7bcfb37f6647bfd9e6193ad818
Thomas Kreek <Thomas@bobandthomas.com>
2013-11-29 10:57:23 -0500

    Updating missing files - SpatialDataSelector and Logger

---
+++ MorbidJava/src/com/bobandthomas/Morbid/UI/Logger.java
@@ -0,0 +1,108 @@
+package com.bobandthomas.Morbid.UI;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.bobandthomas.Morbid.utils.CLoadableItem;
+
+public class Logger {
+	public enum MessageLevel
+	{
+		DEBUG ("Debug"),
+		INFORMATION ("Information"),
+		WARNING ("Warning"),
+		ERROR ("ERROR"),
+		FATAL ("*FATAL*");
+		MessageLevel(String s)
+		{
+			
+		}
+	}
+	protected class Message
+	{
+		CLoadableItem obj;
+		String message;
+		MessageLevel level;
+		public Message(CLoadableItem obj, String message, MessageLevel level) {
+			super();
+			this.obj = obj;
+			this.message = message;
+			this.level = level;
+		}
+		public String shortReport()
+		{
+			return message;
+		}
+		public String report()
+		{
+			String s = obj.getName()+" says:\n" + message + " as " + level.toString();
+			return s;
+		}
+	}
+	int lastReported;
+	class MessageList extends ArrayList<Message>
+	{
+	}
+	MessageList messages;
+	HashMap<Object, MessageList> messageMap;
+	static Logger cpi = null;
+	
+	static Logger get()
+	{
+		if (cpi == null)
+			cpi = new Logger();
+		return cpi;
+	}
+
+	public Logger() {
+		lastReported = 0;
+		messages = new MessageList();
+		messageMap = new HashMap<Object,MessageList>();
+		
+	}
+	public static void addMessage(String message)
+	{
+		get().createMessage(null, message, MessageLevel.INFORMATION);
+	}
+	public static void addMessage(CLoadableItem o, String message)
+	{
+		get().createMessage(o, message, MessageLevel.INFORMATION);
+	}
+	public static void addMessage(CLoadableItem o, String message, MessageLevel ml)
+	{
+		get().createMessage(o,message,ml);
+	}
+	public static void addMessage(CLoadableItem o)
+	{
+		get().createMessage(o, o.toString(), MessageLevel.INFORMATION);
+	}
+	public void createMessage(CLoadableItem o, String message, MessageLevel ml)
+	{
+		Message m = new Logger.Message(o,message,ml);
+		messages.add(m);	
+		MessageList mlist;
+		if ((mlist = messageMap.get(o)) == null)
+		{
+			mlist = new MessageList();
+			messageMap.put(o, mlist);
+		}
+		mlist.add(m);
+		reportNext();
+	}
+	public void reportAll()
+	{
+		lastReported = -1;
+		for (Message m : messages)
+		{
+			lastReported ++;
+			System.out.println(m.report());
+		}
+	}
+	public void reportNext()
+	{
+		for (; lastReported < messages.size(); lastReported++)
+		{
+			System.out.println(messages.get(lastReported).report());
+		}
+	}
+}
---
+++ MorbidJava/src/com/bobandthomas/Morbid/molecule/data/control/SpatialDataSelector.java
@@ -0,0 +1,64 @@
+package com.bobandthomas.Morbid.molecule.data.control;
+
+import javax.swing.JPanel;
+
+import com.bobandthomas.Morbid.Gadget.GadgetSpatialData;
+import com.bobandthomas.Morbid.UI.ControlPanel;
+import com.bobandthomas.Morbid.molecule.data.SpatialData;
+import com.bobandthomas.Morbid.molecule.data.SpatialDataList;
+import com.bobandthomas.Morbid.utils.MorbidEvent;
+
+public class SpatialDataSelector extends ControlPanel {
+
+	SpatialDataList sdl;
+	GadgetSpatialData gsd;
+	SpatialData sd;
+	SpatialDataControl sdc;
+	public SpatialDataSelector(GadgetSpatialData gsd, SpatialDataList sdl, String name, JPanel parentPanel) {
+		super(name, false);
+		if (parentPanel != null)
+		{
+			this.tempChild = child;
+			child = parentPanel;
+		}
+		this.gsd = gsd;
+		this.sdl = sdl;
+		sd = gsd.getSd();
+//		this.sideBySide();
+		createCombo(sdl, "SpatialData", 0);
+		setupSpatialData(sd);
+//		this.endSideBySide();
+	}
+	
+	private void setupSpatialData(SpatialData spatial)
+	{
+		if (sdc != null)
+		{
+			child.remove(sdc);
+			sdc.unRegisterFromAll();
+		}
+		sdc = spatial.getControlPanel(child);
+		sdc.registerListener(this);
+		sd = spatial;
+		child.add(sdc);
+		gsd.setSd(spatial);
+		
+	}
+
+	@Override
+	public void changeValue(String label, Integer value) {
+		if (label.equals("SpatialData"))
+		{
+			setupSpatialData(sdl.get(value));	
+			gsd.markDirty();
+		}
+	}
+
+	@Override
+	public MorbidEvent handleNotify(MorbidEvent source) {
+		gsd.markDirty();
+		return super.handleNotify(source);
+	}
+	
+
+}
